35a1f7980112e7422220dceabb9a6e5624a36947
Anilcan Cakir <anilcan.cakir@gmail.com>
2025-01-29 17:23:09 +0300

    Added gtag.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,22 @@
         href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         rel="stylesheet">
     @vite('resources/css/app.css')
+
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5VDJPVRF');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
 
@@ -108,5 +124,12 @@
 </div>
 
 @vite('resources/js/app.js')
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VDJPVRF"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
